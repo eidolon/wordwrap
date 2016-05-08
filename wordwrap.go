ba7a9dc87995513f5a56980ce475d34d532ea6b2
Elliot Wright <wright.elliot@gmail.com>
2016-05-08 15:55:23 +0100

    Working wrapping and breaking.

--- wordwrap.go
+++ wordwrap.go
@@ -1,5 +1,10 @@
 package wordwrap
 
+import (
+	"bytes"
+	"strings"
+)
+
 // WrapperFunc takes a given input string, and returns some wrapped output. The wrapping may
 // be altered by currying the wrapper function.
 type WrapperFunc func(string) string
@@ -14,9 +19,46 @@ type WrapperFunc func(string) string
 //
 //  wrapper := wordwrap.Wrapper(10, false)
 //  wrapped := wrapper("This string would be split onto several new lines")
-func Wrapper(limit uint, breakWords bool) WrapperFunc {
+func Wrapper(limit int, breakWords bool) WrapperFunc {
+	if limit < 1 {
+		panic("Wrapper limit cannot be less than 1.")
+	}
+
 	return func(input string) string {
-		return ""
+		var wrapped string
+
+		// Split string into array of words
+		words := strings.Fields(input)
+
+		if len(words) == 0 {
+			return wrapped
+		}
+
+		remaining := limit
+
+		if breakWords {
+			words = doBreakWords(words, limit)
+		}
+
+		for _, word := range words {
+			if len(word)+1 > remaining {
+				if len(wrapped) > 0 {
+					wrapped += "\n"
+				}
+
+				wrapped += word
+				remaining = limit - len(word)
+			} else {
+				if len(wrapped) > 0 {
+					wrapped += " "
+				}
+
+				wrapped += word
+				remaining = remaining - (len(word) + 1)
+			}
+		}
+
+		return wrapped
 	}
 }
 
@@ -24,4 +66,40 @@ func Wrapper(limit uint, breakWords bool) WrapperFunc {
 func Indent(input string, prefix string) string {
 	// @todo: implement
 	return ""
+}
+
+// Break up any words in a given array of words that exceed the given limit.
+func doBreakWords(words []string, limit int) []string {
+	var result []string
+
+	for _, word := range words {
+		if len(word) > limit {
+			var parts []string
+			var partBuf bytes.Buffer
+
+			for _, char := range word {
+				atLimit := partBuf.Len() == limit
+
+				if atLimit {
+					parts = append(parts, partBuf.String())
+
+					partBuf.Reset()
+				}
+
+				partBuf.WriteRune(char)
+			}
+
+			if partBuf.Len() > 0 {
+				parts = append(parts, partBuf.String())
+			}
+
+			for _, part := range parts {
+				result = append(result, part)
+			}
+		} else {
+			result = append(result, word)
+		}
+	}
+
+	return result
 }
--- wordwrap_test.go
+++ wordwrap_test.go
@@ -8,29 +8,87 @@ import (
 
 func TestWrapper(t *testing.T) {
 	tests := []struct {
-		limit      uint
+		limit      int
 		breakWords bool
 		input      string
 		expected   string
 	}{
+		// When not breaking words
+		// Should wrap text so words fit on lines with the given limit
 		{
 			10,
 			false,
 			"Test text Test text Test text Test text",
 			"Test text\nTest text\nTest text\nTest text",
 		},
+		// Should remove additional whitespace
+		{
+			10,
+			false,
+			"Test  text  Test  text  Test  text  Test  text",
+			"Test text\nTest text\nTest text\nTest text",
+		},
+		// Should trim text
+		{
+			10,
+			false,
+			"    Test  text  Test  text  Test  text  Test  text    ",
+			"Test text\nTest text\nTest text\nTest text",
+		},
+		// Should not break words if breakWords is false
+		{
+			4,
+			false,
+			"Testtext",
+			"Testtext",
+		},
+		// When breaking words
+		// Should break words, and insert a hyphen
+		{
+			4,
+			true,
+			"Testtext",
+			"Test\ntext",
+		},
+		// Should break words into tiny pieces
+		{
+			1,
+			true,
+			"Testtext",
+			"T\ne\ns\nt\nt\ne\nx\nt",
+		},
+		// Slightly more realistic data
+		// Should be broken properly
+		{
+			40,
+			false,
+			"This is a bunch of text that should be split at somewhere near 40 characters.",
+			"This is a bunch of text that should be\nsplit at somewhere near 40 characters.",
+		},
 	}
 
-	for _, test := range tests {
+	for i, test := range tests {
 		wrapper := wordwrap.Wrapper(test.limit, test.breakWords)
 		wrapped := wrapper(test.input)
 
 		if wrapped != test.expected {
 			t.Fatalf(
-				"Wrapper did not return expected result.\nExpected:\n%s\nActual:\n%s\n",
+				"Wrapper for case %d did not return expected result.\nExpected:\n%s\nActual:\n%s\n",
+				i+1,
 				test.expected,
 				wrapped,
 			)
 		}
 	}
+}
+
+func TestWrapperPanicsWithInvalidLimit(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatalf("Wrapper did not panic on invalid input.")
+		}
+	}()
+
+	// 0 is an invalid limit
+	wordwrap.Wrapper(0, false)
 }
